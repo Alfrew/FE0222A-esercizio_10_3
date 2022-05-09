35c8b932d126dc98809ed276166cd811508a6d7d
Alfrew <filigheddualfredo@gmail.com>
2022-05-09 12:26:41 +0200

    aggiunta di navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer.component';
+import { Route, RouterModule } from '@angular/router';
+import { UsersPage } from './pages/users.page';
 
+const routes: Route[] = [
+    {
+        path: 'users',
+        component: UsersPage,
+    },
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, NavbarComponent, FooterComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/users.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `
+    <p>
+      users works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UsersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
